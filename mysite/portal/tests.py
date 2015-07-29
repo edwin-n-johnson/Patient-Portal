0d8170864cd5bfe9875709caaf33ef104a055b1f
edwin-n-johnson <edwin_n_johnson@yahoo.com>
2015-07-29 08:42:45 -0600

    Creating placeholder for model tests

--- mysite/portal/tests.py
+++ mysite/portal/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Patient
 
 # Create your tests here.
+class PatientTestCase(TestCase):
+    def setUp(self):
+        print "PatientTestCase::setUp()"
+
+    def test_placeholder(self):
+        print "PatientTestCase::test_placeholder()"
+        self.assertTrue(True)
