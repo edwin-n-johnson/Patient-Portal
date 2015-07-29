f377476f7e97dd90408a94467f52799d890ead34
edwin-n-johnson <edwin_n_johnson@yahoo.com>
2015-07-29 09:34:45 -0600

    Add more models, migrate and add examples to database

--- mysite/portal/admin.py
+++ mysite/portal/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Patient,Visit
+from .models import Patient,Physician,Appointment,Message
 
 admin.site.register(Patient)
-admin.site.register(Visit)
+admin.site.register(Physician)
+admin.site.register(Appointment)
+admin.site.register(Message)
--- mysite/portal/models.py
+++ mysite/portal/models.py
@@ -11,13 +11,36 @@ class Patient(models.Model):
     def __unicode__(self):
         return self.lastname + ',' + self.firstname
 
+class Physician(models.Model):
+    firstname = models.CharField(max_length=30)
+    lastname  = models.CharField(max_length=30)
+
+    DOCTOR    = 'DR'
+    PHYSASST  = 'PA'
+    NURSEPR   = 'NP'
+    PHYSICIAN_TYPE_CHOICES = (
+        (DOCTOR,    'Doctor'),
+        (PHYSASST,  'Physician Assistant'),
+        (NURSEPR,   'Nurse Practitioner'),
+        )
+    phystype  = models.CharField(max_length=max(len(x[0]) for x in PHYSICIAN_TYPE_CHOICES),
+                                 choices=PHYSICIAN_TYPE_CHOICES,
+                                 default=DOCTOR)
+    
 # Visit class
-class Visit(models.Model):
+class Appointment(models.Model):
     patient   = models.ForeignKey(Patient)
+    physician = models.ForeignKey(Physician)
     date      = models.DateField()
     summary   = models.CharField(max_length=256)
-
+    
     def __unicode__(self):
-        return unicode(self.date) + ': ' + self.summary
+        return unicode(self.date) + ' with ' + self.physician
 
 
+class Message(models.Model):
+    patient   = models.ForeignKey(Patient)
+    date      = models.DateField()
+    subject   = models.CharField(max_length=50)
+    body      = models.CharField(max_length=2048)
+    
